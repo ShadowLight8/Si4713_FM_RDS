cbd8eda36fd399d2614ea765346b6a2c4bad9479
ShadowLight8 <stuff@nickandbeth.com>
2018-03-27 23:40:18 -0400

    Working from fppd callback and updater startup

---
+++ Si4713_RDS_Updater.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+from sys import argv
+from time import sleep
+import logging
+import json
+import os
+import errno
+import atexit
+import socket
+
+script_dir = os.path.dirname(os.path.abspath(argv[0]))
+
+logging.basicConfig(filename=os.path.dirname(os.path.abspath(argv[0]))+'/Si4713_RDS_Updater.log',level=logging.DEBUG)
+logging.debug("----------")
+logging.debug("Arguments %s", argv[1:])
+
+fifo_path = script_dir + "/Si4713_FM_RDS_FIFO"
+updater_name = "Si4713_RDS_Updater.py"
+
+# Establish lock via socket
+lock_socket = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+lock_socket.bind('\0Si4713_RDS_Updater')
+
+# Config from os.path.abspath(argv[0])
+# ../../config/plugin.Si4713_FM_RDS
+# TODO: Not sure this is the right way to locate the config file, but it works
+
+@atexit.register
+def cleanup():
+	try:
+		os.unlink(fifo_path)
+	except:
+		pass
+
+try:
+	os.mkfifo(fifo_path)
+except OSError as oe:
+	if oe.errno != errno.EEXIST:
+		raise
+
+with open(fifo_path, 'r') as fifo:
+	while True:
+		line = fifo.read()
+		if len(line) > 0:
+			logging.debug(line)
+			if line == "EXIT":
+				exit()
+		else:
+			#print "Empty line"
+			sleep(1)
--- callbacks.py
+++ callbacks.py
@@ -1,23 +1,74 @@
-#!/usr/bin/env python
+#!/usr/bin/python2.7
 
 from sys import argv
+from time import sleep
 import logging
 import json
 import os
+import errno
+import subprocess
+import socket
 
-logging.basicConfig(filename=os.path.dirname(os.path.abspath(argv[0]))+'/Si4713_FM_RDS.log',level=logging.DEBUG)
+script_dir = os.path.dirname(os.path.abspath(argv[0]))
+
+logging.basicConfig(filename=script_dir+'/Si4713_FM_RDS.log',level=logging.DEBUG)
 logging.debug("----------")
 logging.debug("Arguments %s", argv[1:])
+
+fifo_path = script_dir + "/Si4713_FM_RDS_FIFO"
+updater_name = "Si4713_RDS_Updater.py"
 
 # Config from os.path.abspath(argv[0])
 # ../../config/plugin.Si4713_FM_RDS
 # TODO: Not sure this is the right way to locate the config file, but it works
 
+# TODO: If RDS is not enabled, exit?
+
 if len(argv) > 1:
 	if argv[1] == "--list":
-		# TODO: If configured, reset and startup Si4713 prior to returning
+		#sleep(5)
+		logging.debug("Processing --list");
+		try:
+			logging.debug("Checking for socket lock")
+			lock_socket = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+			lock_socket.bind('\0Si4713_RDS_Updater')
+			lock_socket.close()
+			logging.debug("Lock not found")
+			logging.debug("Starting " + script_dir + "/" + updater_name)
+
+			devnull = open(os.devnull, 'w')
+			subprocess.Popen(["python",script_dir + "/" + updater_name], stdin=devnull, stdout=devnull, stderr=devnull)
+		except socket.error:
+			logging.debug("Lock found - " + script_dir + "/" + updater_name + " is running")
+
+		try:
+			logging.debug("Setting up write side of fifo " + fifo_path)
+			os.mkfifo(fifo_path)
+		except OSError as oe:
+			logging.debug("Already exists")
+			if oe.errno != errno.EEXIST:
+				raise
+
+		with open(fifo_path, 'w') as fifo:
+			fifo.write("INIT")
+			fifo.flush()
+
 		print "media,playlist"
+		#sleep(5)
 		exit()
+	elif argv[1] == "--exit":
+		try:
+			logging.debug("Setting up write side of fifo " + fifo_path)
+			os.mkfifo(fifo_path)
+		except OSError as oe:
+			logging.debug("Already exists")
+			if oe.errno != errno.EEXIST:
+				raise
+
+		with open(fifo_path, 'w') as fifo:
+			fifo.write("EXIT")
+			fifo.flush()
+
 	elif argv[2] == "media":
 		# TODO: When not type:pause or event?
 		#	If RDS on, kill existing RDS script, kick off new RDS script to run for item duration
