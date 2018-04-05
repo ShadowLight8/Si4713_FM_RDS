2fb9bb3410a564ea1d4c4dd090c4b81b039fcabb
ShadowLight8 <stuff@nickandbeth.com>
2018-04-04 23:42:52 -0400

    Some cleanup to the updater

--- Si4713_RDS_Updater.py
+++ Si4713_RDS_Updater.py
@@ -13,12 +13,10 @@ import socket
 
 script_dir = os.path.dirname(os.path.abspath(argv[0]))
 
-logging.basicConfig(filename=os.path.dirname(os.path.abspath(argv[0]))+'/Si4713_RDS_Updater.log',level=logging.DEBUG,format='%(asctime)s:%(name)s:%(levelname)s:%(message)s')
-logging.debug("----------")
-logging.debug("Arguments %s", argv[1:])
+logging.basicConfig(filename=os.path.dirname(os.path.abspath(argv[0]))+'/Si4713_RDS_Updater.log',level=logging.INFO,format='%(asctime)s:%(name)s:%(levelname)s:%(message)s')
+logging.info("----------")
 
 fifo_path = script_dir + "/Si4713_FM_RDS_FIFO"
-updater_name = "Si4713_RDS_Updater.py"
 
 inited = False
 stationdata = ' Happy  Hallo-     -ween'
@@ -63,56 +61,55 @@ with open(fifo_path, 'r', 0) as fifo:
 	stationfrag = 0
 	buffertick = 0
 	while True:
-		line = fifo.readline()
-		line = line.rstrip()
+		line = fifo.readline().rstrip()
 		if len(line) > 0:
 			logging.debug('line - ' + line)
 			if line == 'EXIT':
-				logging.debug('exit')
+				logging.info('exit')
 				exit()
 			elif line == 'INIT':
-				logging.debug('init')
+				logging.info('init')
 				# TODO: Reset Si4713, configure FM, configure blank RDS
 				if not radio.begin():
 					logging.error('Unable to initialize radio. Check that the Si4713 is connected, then restart FPPD.')
+					exit(-1)
 				radio.setTXpower(100)
 				radio.tuneFM(10010)
 				radio.beginRDS()
 				radio.setRDSstation(stationdata[0:8])
 				inited = True
+				logging.info('Radio initialized')
 			elif line == 'START':
-				logging.debug('start')
+				logging.info('start')
 				# TODO: ?
 			elif line[0] == 'T':
 				logging.debug('T')
 				title = line[1:33]
+				# Reset RDS buffer and tick to update when title changes
+				logging.info('Reset buffer to title [' + title + ']')
 				radio.setRDSbuffer(title)
+				buffertick = 0
 			elif line[0] == 'A':
 				logging.debug('A')
 				artist = line[1:33]
 			else:
 				# TODO: ?
-				logging.debug('else')				
-		else:
-			if len(title) > 0:
-				logging.debug('T-' + title)
-			if len(artist) > 0:
-				logging.debug('A-' + artist)
+				logging.info('else')
 
+		else:
 			stationtick = (stationtick + 1) % stationdelay
 			if inited and stationtick == 0:
-				logging.debug('Updating RDS station with fragment ' + str(stationfrag) + ' [' + stationdata[8*stationfrag:8*(stationfrag+1)] + ']')
+				logging.info('Set station to fragment ' + str(stationfrag) + ' [' + stationdata[8*stationfrag:8*(stationfrag+1)] + ']')
 				radio.setRDSstation(stationdata[8*stationfrag:8*(stationfrag+1)])
 				stationfrag = (stationfrag + 1) % stationfragments
-			logging.debug ('Tick - ' + str(stationtick) + ' Frag - ' + str(stationfrag))
 
 			buffertick = (buffertick + 1) % bufferdelay
 			if inited and buffertick == 0:
 				if showartist:
-					logging.debug('Set buffer to artist ' + artist)
+					logging.info('Set buffer to artist [' + artist + ']')
 					radio.setRDSbuffer(artist)
 				else:
-					logging.debug('Set buffer to title ' + title)
+					logging.info('Set buffer to title [' + title + ']')
 					radio.setRDSbuffer(title)
 				showartist = not showartist
 
